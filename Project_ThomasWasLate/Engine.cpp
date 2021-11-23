e058348212ce37eadbf4ec33ea8f0a918931218a
Ethan Rahman <A0092411@ayrshire.ac.uk>
2021-11-23 12:37:06 +0000

    Created Draw, Input and Update.cpp files and added code

--- Project_ThomasWasLate/Engine.cpp
+++ Project_ThomasWasLate/Engine.cpp
@@ -1,20 +1,56 @@
 #include "Engine.h"
-void Engine::input()
-{
-}
-
-void Engine::update(float dtAsSeconds)
-{
-}
-
-void Engine::draw()
-{
-}
-
 Engine::Engine()
 {
+	//Get the screen resolution 
+	//and create an SFML window and view 
+	sf::Vector2f resolution;
+	resolution.x = sf::VideoMode::getDesktopMode().width;
+	resolution.y = sf::VideoMode::getDesktopMode().height;
+	m_Window.create(sf::VideoMode(resolution.x, resolution.y),
+		"Thomas was late",
+		sf::Style::Fullscreen);
+
+	//Initialize the full screen view 
+	m_MainView.setSize(resolution);
+	m_HUDView.reset(
+		sf::FloatRect(0, 0, resolution.x, resolution.y));
+
+	//Initialize the split-screen views
+	m_LeftView.setViewport(
+		sf::FloatRect(0.001f, 0.001f, 0.498f, 0.998f));
+
+	m_RightView.setViewport(
+		sf::FloatRect(0.5f, 0.001f, 0.499f, 0.998f));
+
+	m_BGLeftView.setViewport(
+		sf::FloatRect(0.001f, 0.001f, 0.498f, 0.998f));
+
+	m_BGRightView.setViewport(
+		sf::FloatRect(0.5f, 0.001f, 0.499f, 0.998f));
+
+	m_BackgroundTexture = TextureHolder::GetTexture("assets/graphics/background.png");
+
+	//Asociate the sprite with the texture 
+	m_BackgroundSprite.setTexture(m_BackgroundTexture);
 }
 
 void Engine::run()
 {
+	//Timing 
+	sf::Clock clock;
+
+	while (m_Window.isOpen())
+	{
+		sf::Time dt = clock.restart();
+		//Update the total game time 
+		m_GameTimeTotal += dt;
+
+		//Make a decimal fraction from the delta time 
+		float dtAsSeconds = dt.asSeconds();
+
+		//Call each part of the game loop in turn 
+		input();
+		update(dtAsSeconds);
+		draw();
+	}
 }
--- Project_ThomasWasLate/Engine.h
+++ Project_ThomasWasLate/Engine.h
@@ -5,7 +5,7 @@
 
 class Engine
 {
-private
+private:
 	//Texture Holder 
 	TextureHolder th;
 	const int TILE_SIZE = 50;
@@ -14,38 +14,51 @@ private
 	//The force pushing the characters down 
 	const int GRAVITY = 300;
 
+	//A regular RenderWindow
 	sf::RenderWindow m_Window;
 
+	//The main views
+	sf::View m_MainView;
+	sf::View m_LeftView;
+	sf::View m_RightView;
+
+	//Three views for the background
 	sf::View m_BGMainView;
 	sf::View m_BGLeftView;
 	sf::View m_BGRightView;
 	sf::View m_HUDView;
 
+	//Declare a sprite and a texture
 	sf::Sprite m_BackgroundSprite;
 	sf::Texture m_BackgroundTexture;
 
+	//Is the game currently playing
 	bool m_IsPlaying = false;
 
+	//Is character 1 or 2 the current focus
 	bool m_Character1 = true;
 
+	//Start in full screen mode
 	bool m_IsSplitScreen = false;
 
+	//Is it time for a new level 
 	bool m_NewLevelRequired = true;
 
+	//Time left in the current level (seconds)
 	float m_TimeRemaining = 10;
 	sf::Time m_GameTimeTotal;
 
+	//Private functions for internal use only 
 	void input();
 	void update(float dtAsSeconds);
 	void draw();
 
-public
+public:
+	//Engine constructor 
 	Engine();
+	//Run will call all the private functions 
 	void run();
-
-
 };
-
 
 #endif ENGINE_H
 
