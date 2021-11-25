bfd2aad3a77f0258b3030de391c171dda3eac746
Ethan Rahman <A0092411@ayrshire.ac.uk>
2021-11-25 21:56:52 +0000

    Created Bob.h, Thomas.h and Bob.cpp and Thomas.cpp and added code

---
+++ Project_ThomasWasLate/Bob.h
@@ -0,0 +1,15 @@
+#ifndef BOB_H
+#define BOB_H
+#include "PlayableCharacter.h"
+
+class Bob : public PlayableCharacter
+{
+public:
+	//A constructor specific to Bob 
+	Bob();
+	//The overridden input handler for Bob 
+	bool virtual handleInput();
+};
+
+#endif BOB_H
+
